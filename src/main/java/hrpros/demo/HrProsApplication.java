8684aa5940b8126f60edf84dea1f70673ada9c58
Yan Wenjie <yanwenjie@zidanduanxin.com>
2018-09-21 21:25:00 +0800

    test

---
+++ src/main/java/hrpros/demo/HrProsApplication.java
@@ -0,0 +1,12 @@
+package hrpros.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrProsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrProsApplication.class, args);
+    }
+}
