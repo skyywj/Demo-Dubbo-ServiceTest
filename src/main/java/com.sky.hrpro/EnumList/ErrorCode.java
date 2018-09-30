ee45ab8b8b9d3f8524842128da14a298c705ea98
Yan Wenjie <yanwenjie@zidanduanxin.com>
2018-09-30 14:24:43 +0800

    add errorcode enum

---
+++ src/main/java/com.sky.hrpro/EnumList/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.sky.hrpro.EnumList;
+
+/**
+ * Desc:错误码定义
+ *
+ * @Author: CarryJey
+ * @Date: 2018/9/30 14:17:11
+ */
+public enum ErrorCode {
+    //服务端错误
+    ERROR_SERVER_ERROR(0),
+    //超时错误
+    ERROR_TIME_OUT(1);
+
+
+    private final int value;
+    private ErrorCode(int value){
+        this.value = value;
+    }
+}
