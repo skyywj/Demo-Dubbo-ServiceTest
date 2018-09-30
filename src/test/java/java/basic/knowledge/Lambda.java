a19c2526b7704e452af6adcbd8ac706740b88f48
Yan Wenjie <yanwenjie@zidanduanxin.com>
2018-09-30 14:05:32 +0800

    test lambda

---
+++ src/test/java/java/basic/knowledge/Lambda.java
@@ -0,0 +1,41 @@
+package java.basic.knowledge;
+
+/**
+ * Desc:lambda表达式练习
+ *
+ * @Author: CarryJey
+ * @Date: 2018/9/30 13:46:16
+ */
+public class Lambda {
+
+    /**
+     * 参数传递是以Runnable的方式进行
+     * @param a
+     */
+    public void test(Runnable a){
+        a.run();
+    }
+
+    public static void main(String args[]) {
+
+        Lambda lambda = new Lambda();
+
+        lambda.test(
+                () -> {
+            System.out.println("a");
+            System.out.println("b"); }
+        );
+
+        /**
+         * 下面是另外两种写法
+         */
+//        Runnable r = () -> { System.out.println("lambda testing...");
+//        System.out.println("test02");};
+//        r.run();
+
+//        new Thread(()->{System.out.println("a1");
+//        System.out.println("a2");}).run();
+
+
+    }
+}
